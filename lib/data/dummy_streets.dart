9dda9fd4f56355f5fbcfe09ac7e146059cebcb7b
Ales Zima <ales@cpex.cz>
2023-10-06 20:36:38 +0200

    Updated dummyStreets

--- lib/data/dummy_streets.dart
+++ lib/data/dummy_streets.dart
@@ -6,35 +6,56 @@ const dummyStreet = [
     id: 0,
     name: 'J. Holuba',
     imageUrl:
-        'https://firebasestorage.googleapis.com/v0/b/kralupy-streets.appspot.com/o/street_images%2Fnadrazni.jpg?alt=media&token=ca0e68e5-1531-44f4-a8fd-06aca30d159d&_gl=1*1dr5e32*_ga*ODgyMzc0OTUuMTY4NDkzNjkzNQ..*_ga_CW55HF8NVT*MTY5NjU5NTIxOS4zOS4xLjE2OTY1OTU3MjQuMjQuMC4w',
+        'https://firebasestorage.googleapis.com/v0/b/kralupy-streets.appspot.com/o/street_images%2Fj_holuba.jpg?alt=media&token=3ea82fe7-687f-443c-a5a7-be8b75360e90&_gl=1*t45qw7*_ga*ODgyMzc0OTUuMTY4NDkzNjkzNQ..*_ga_CW55HF8NVT*MTY5NjYxNjE4Ni40MC4xLjE2OTY2MTY5MDUuNTguMC4w',
     geolocation: Geolocation(latitude: 50.246103, longitude: 14.312623),
   ),
   Street(
     id: 1,
     name: 'Nábřeží J. Rysa',
     imageUrl:
-        'https://firebasestorage.googleapis.com/v0/b/kralupy-streets.appspot.com/o/street_images%2Fnadrazni.jpg?alt=media&token=ca0e68e5-1531-44f4-a8fd-06aca30d159d&_gl=1*1dr5e32*_ga*ODgyMzc0OTUuMTY4NDkzNjkzNQ..*_ga_CW55HF8NVT*MTY5NjU5NTIxOS4zOS4xLjE2OTY1OTU3MjQuMjQuMC4w',
-    geolocation: Geolocation(latitude: 50.245001, longitude: 14.314869),
+        'https://firebasestorage.googleapis.com/v0/b/kralupy-streets.appspot.com/o/street_images%2Fj_rysa.jpg?alt=media&token=42fdf3f2-f417-4055-8443-9427792932db&_gl=1*77cpo1*_ga*ODgyMzc0OTUuMTY4NDkzNjkzNQ..*_ga_CW55HF8NVT*MTY5NjYxNjE4Ni40MC4xLjE2OTY2MTY5MzEuMzIuMC4w',
+    geolocation: Geolocation(latitude: 50.243754, longitude: 14.317525),
   ),
   Street(
     id: 2,
-    name: 'Maxima Gorkého',
+    name: 'Alšova',
     imageUrl:
-        'https://firebasestorage.googleapis.com/v0/b/kralupy-streets.appspot.com/o/street_images%2Fzizkova.jpg?alt=media&token=7067d26f-244e-41aa-8098-781d74e8c3bd&_gl=1*plhtua*_ga*ODgyMzc0OTUuMTY4NDkzNjkzNQ..*_ga_CW55HF8NVT*MTY5NjU5NTIxOS4zOS4xLjE2OTY1OTU4NzkuMTMuMC4w',
-    geolocation: Geolocation(latitude: 50.248047, longitude: 14.309234),
+        'https://firebasestorage.googleapis.com/v0/b/kralupy-streets.appspot.com/o/street_images%2Falsova.jpg?alt=media&token=fea39097-87d2-4883-bde0-4089973d2580&_gl=1*5ylvys*_ga*ODgyMzc0OTUuMTY4NDkzNjkzNQ..*_ga_CW55HF8NVT*MTY5NjYxNjE4Ni40MC4xLjE2OTY2MTcwOTEuNjAuMC4w',
+    geolocation: Geolocation(latitude: 50.244561, longitude: 14.316177),
   ),
   Street(
     id: 3,
-    name: 'Raisova',
+    name: 'Máchova',
     imageUrl:
-        'https://firebasestorage.googleapis.com/v0/b/kralupy-streets.appspot.com/o/street_images%2Fzizkova.jpg?alt=media&token=7067d26f-244e-41aa-8098-781d74e8c3bd&_gl=1*plhtua*_ga*ODgyMzc0OTUuMTY4NDkzNjkzNQ..*_ga_CW55HF8NVT*MTY5NjU5NTIxOS4zOS4xLjE2OTY1OTU4NzkuMTMuMC4w',
-    geolocation: Geolocation(latitude: 50.2470139, longitude: 14.3101673),
+        'https://firebasestorage.googleapis.com/v0/b/kralupy-streets.appspot.com/o/street_images%2Fmachova.jpg?alt=media&token=ed24bbcf-5a0c-4c6d-972c-4cb1e7f92084&_gl=1*1oox5gx*_ga*ODgyMzc0OTUuMTY4NDkzNjkzNQ..*_ga_CW55HF8NVT*MTY5NjYxNjE4Ni40MC4xLjE2OTY2MTcxMTcuMzQuMC4w',
+    geolocation: Geolocation(latitude: 50.243771, longitude: 14.317632),
   ),
   Street(
     id: 4,
-    name: 'Tomáškova',
+    name: 'Šrámkova',
     imageUrl:
-        'https://firebasestorage.googleapis.com/v0/b/kralupy-streets.appspot.com/o/street_images%2Fzizkova.jpg?alt=media&token=7067d26f-244e-41aa-8098-781d74e8c3bd&_gl=1*plhtua*_ga*ODgyMzc0OTUuMTY4NDkzNjkzNQ..*_ga_CW55HF8NVT*MTY5NjU5NTIxOS4zOS4xLjE2OTY1OTU4NzkuMTMuMC4w',
-    geolocation: Geolocation(latitude: 50.247459, longitude: 14.310229),
+        'https://firebasestorage.googleapis.com/v0/b/kralupy-streets.appspot.com/o/street_images%2Fsramkova.jpg?alt=media&token=94f94071-8ff2-41ab-ae52-329cd3bca50f&_gl=1*s8nd10*_ga*ODgyMzc0OTUuMTY4NDkzNjkzNQ..*_ga_CW55HF8NVT*MTY5NjYxNjE4Ni40MC4xLjE2OTY2MTcxNjIuNjAuMC4w',
+    geolocation: Geolocation(latitude: 50.243699, longitude: 14.320376),
+  ),
+  Street(
+    id: 5,
+    name: 'Makarenkova',
+    imageUrl:
+        'https://firebasestorage.googleapis.com/v0/b/kralupy-streets.appspot.com/o/street_images%2Fmakarenkova.jpg?alt=media&token=cf8f2821-a5bd-476f-ab46-109cd47810fd&_gl=1*9me69k*_ga*ODgyMzc0OTUuMTY4NDkzNjkzNQ..*_ga_CW55HF8NVT*MTY5NjYxNjE4Ni40MC4xLjE2OTY2MTcyMzYuNjAuMC4w',
+    geolocation: Geolocation(latitude: 50.245224, longitude: 14.320096),
+  ),
+  Street(
+    id: 6,
+    name: 'Kyjevská',
+    imageUrl:
+        'https://firebasestorage.googleapis.com/v0/b/kralupy-streets.appspot.com/o/street_images%2Fkyjevska.jpg?alt=media&token=ef77012c-b685-4ce0-821e-6d8c00ff5af1&_gl=1*1aoo0mm*_ga*ODgyMzc0OTUuMTY4NDkzNjkzNQ..*_ga_CW55HF8NVT*MTY5NjYxNjE4Ni40MC4xLjE2OTY2MTczMjUuNjAuMC4w',
+    geolocation: Geolocation(latitude: 50.246203, longitude: 14.318211),
+  ),
+  Street(
+    id: 7,
+    name: 'Nám. J. Seiferta',
+    imageUrl:
+        'https://firebasestorage.googleapis.com/v0/b/kralupy-streets.appspot.com/o/street_images%2Fj_seiferta.jpg?alt=media&token=6e6fde68-5835-4a23-b987-17105b74fff6&_gl=1*u4g9s6*_ga*ODgyMzc0OTUuMTY4NDkzNjkzNQ..*_ga_CW55HF8NVT*MTY5NjYxNjE4Ni40MC4xLjE2OTY2MTczMzQuNTEuMC4w',
+    geolocation: Geolocation(latitude: 50.247764, longitude: 14.317231),
   ),
 ];
