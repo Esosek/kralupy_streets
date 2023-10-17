e3dd224835d5a39c5311abc90b0d045c0da81d76
Ales Zima <ales@cpex.cz>
2023-10-17 20:37:12 +0200

    StreetDetailScreen landscape

--- lib/screens/street_detail_screen.dart
+++ lib/screens/street_detail_screen.dart
@@ -23,46 +23,85 @@ class StreetDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     _logVisit();
+    bool isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
     final lat = street.geolocation.latitude;
     final lng = street.geolocation.longitude;
+
+    Widget mapPreview = Expanded(
+      child: Container(
+        color: const Color.fromRGBO(224, 224, 224, 1),
+        width: double.infinity,
+        child: Image.network(
+          'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=17&size=600x300&maptype=roadmap&markers=color:red%7C$lat,$lng&key=$googleApiKey',
+          fit: BoxFit.fitWidth,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) {
+              return child;
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+      ),
+    );
+
+    Widget content = Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        mapPreview,
+        if (street.descriptionParagraphs != null)
+          for (String paragraph in street.descriptionParagraphs!)
+            Padding(
+              padding: const EdgeInsets.only(top: 12),
+              child: Text(
+                paragraph,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+        const SizedBox(height: 16),
+        Image.network(street.imageUrl),
+      ],
+    );
+
+    if (isLandscape) {
+      content = Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          if (street.descriptionParagraphs != null)
+            for (String paragraph in street.descriptionParagraphs!)
+              Padding(
+                padding: const EdgeInsets.only(top: 12),
+                child: Text(
+                  paragraph,
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ),
+          const SizedBox(height: 16),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Image.network(street.imageUrl),
+              ),
+              const SizedBox(width: 20),
+              mapPreview,
+            ],
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(street.name),
       ),
       body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              color: Colors.grey.shade300,
-              width: double.infinity,
-              child: Image.network(
-                'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=17&size=600x300&maptype=roadmap&markers=color:red%7C$lat,$lng&key=$googleApiKey',
-                fit: BoxFit.fitWidth,
-                loadingBuilder: (context, child, loadingProgress) {
-                  if (loadingProgress == null) {
-                    return child;
-                  }
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                },
-              ),
-            ),
-            if (street.descriptionParagraphs != null)
-              for (String paragraph in street.descriptionParagraphs!)
-                Padding(
-                  padding: const EdgeInsets.only(top: 12),
-                  child: Text(
-                    paragraph,
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                ),
-            const SizedBox(height: 16),
-            Image.network(street.imageUrl),
-          ],
-        ),
+        padding: isLandscape
+            ? const EdgeInsets.symmetric(vertical: 6, horizontal: 16)
+            : const EdgeInsets.all(16),
+        child: content,
       ),
     );
   }
