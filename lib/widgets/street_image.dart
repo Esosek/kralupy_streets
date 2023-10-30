4ffb10b4a23008d8cd0d5232d5d824dee0b7806f
Ales Zima <ales@cpex.cz>
2023-10-30 08:20:48 +0100

    Optimizing image cache

--- lib/widgets/street_image.dart
+++ lib/widgets/street_image.dart
@@ -16,63 +16,73 @@ class StreetImage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final imagePath = ref.read(imageProvider.notifier).getImagePath(street);
-    final failContainer = Container(
-      color: Colors.grey.shade200,
-      height: 90,
-      width: double.infinity,
-      child: Center(
-        child: condensed
-            ? const Center(
-                child: Icon(
-                  Icons.error_outline_rounded,
-                  color: Colors.grey,
-                ),
-              )
-            : Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.wifi_off_rounded,
+    final images = ref.read(imageProvider);
+    if (images[street.id] != null) {
+      return Image.file(
+        File(images[street.id]!),
+        fit: BoxFit.cover,
+        height: height,
+        width: double.infinity,
+      );
+    } else {
+      final imagePath = ref.read(imageProvider.notifier).getImagePath(street);
+      final failContainer = Container(
+        color: Colors.grey.shade200,
+        height: 90,
+        width: double.infinity,
+        child: Center(
+          child: condensed
+              ? const Center(
+                  child: Icon(
+                    Icons.error_outline_rounded,
                     color: Colors.grey,
                   ),
-                  const SizedBox(width: 8),
-                  Text(
-                    'Nahrávání obrázku selhalo. Zkontrolujte prosím své připojení.',
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.bodySmall,
-                  ),
-                ],
-              ),
-      ),
-    );
-    return FutureBuilder(
-      future: imagePath,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (snapshot.hasError) {
-          return failContainer;
-        } else {
-          if (snapshot.hasData) {
-            final imagePath = snapshot.data;
-            if (imagePath!.isNotEmpty) {
-              return Image.file(
-                File(imagePath),
-                fit: BoxFit.cover,
-                height: height,
-                width: double.infinity,
-              );
+                )
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.wifi_off_rounded,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      'Nahrávání obrázku selhalo. Zkontrolujte prosím své připojení.',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                  ],
+                ),
+        ),
+      );
+      return FutureBuilder(
+        future: imagePath,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return failContainer;
+          } else {
+            if (snapshot.hasData) {
+              final imagePath = snapshot.data;
+              if (imagePath!.isNotEmpty) {
+                return Image.file(
+                  File(imagePath),
+                  fit: BoxFit.cover,
+                  height: height,
+                  width: double.infinity,
+                );
+              } else {
+                return failContainer;
+              }
             } else {
               return failContainer;
             }
-          } else {
-            return failContainer;
           }
-        }
-      },
-    );
+        },
+      );
+    }
   }
 }
